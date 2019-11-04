e4bb6750d8678911d16756d3ccc5bf3733ea26c7
Phil <ph6668si-s@student.lu.se>
2019-11-04 15:38:19 +0100

    deleted day.sh

--- code/day.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-awk '
-     {
-        tspec = sprintf("%4d %.2d %.2d 00 00 00", substr($1,1,4), substr($2,1,2), substr($2,3,4))
-        t = mktime(tspec)
-        $(NF+1) = 0 + strftime("%j",t)
-    } {print}' /Users/philipsiemund/MNXB01-Project-2019/datasets/tempdata_Lund.txt >> /Users/philipsiemund/MNXB01-Project-2019/datasets/tempdata_Lund_days.txt
-
