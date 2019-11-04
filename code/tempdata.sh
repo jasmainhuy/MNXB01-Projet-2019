70083f56cebea8c61db352b2dd26f68490e2d496
estelleser <e.sernoux@gmail.com>
2019-11-04 13:31:47 +0100

    Change path usage to use relative path

--- code/tempdata.sh
+++ code/tempdata.sh
@@ -5,8 +5,10 @@
 
 #Change the DATASET variable if needed and make sure to specify the correct paths! Also, for OSX, the '' after sed -i is apparently mandatory. For other operating systems this might throw an error 
 
-DATASET=Lund
+#DATASET=Lund
+DATASET=Soderarm
 
-awk -F ';' 'x==1 {print $1 " " $2 " " $3 " " $4} /Datum/ {x=1}' /Users/home/MNXB01-Project-2019/datasets/smhi-opendata_${DATASET}.csv >> /Users/home/MNXB01-Project-2019/datasets/tempdata_${DATASET}.txt
+# paths have to be relative from <project folder>/code folder
+awk -F ';' 'x==1 {print $1 " " $2 " " $3 " " $4} /Datum/ {x=1}' ../datasets/smhi-opendata_${DATASET}.csv >> ../datasets/tempdata_${DATASET}.txt
 
-sed -i '' 's/[-:,]//g' /Users/home/MNXB01-Project-2019/datasets/tempdata_${DATASET}.txt
+sed -i 's/[-:,]//g' ../datasets/tempdata_${DATASET}.txt
