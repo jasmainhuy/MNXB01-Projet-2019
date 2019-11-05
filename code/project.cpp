10a124e24050da1d444dd440035dd31d33ac4633
jleygonie <johan.leygonie@gmail.com>
2019-11-05 02:04:04 +0100

    Uptdate on tempTrender.cpp

--- code/project.cpp
+++ code/project.cpp
@@ -2,17 +2,9 @@
 #include <string>
 
 
-void tempOnDay(n){
-	
-	
-}
-
-
 void project() {
 	string pathToFile = "/home/courseuser/Documents/Project/MNXB01-project/datasets/smhi-openda_Karlstad.csn"; //Put the path to your data file here
 	tempTrender t(pathToFile); //Instantiate your analysis object
 	t.tempOnDay(235);
-	
-	
-
+	return 0;
 }
--- code/tempTrender.cpp
+++ code/tempTrender.cpp
@@ -1,7 +1,79 @@
-#include <iostream>
 #include "tempTrender.h"
 
+#include <fstream>
+#include <iostream>
+
+#include <TF1.h> 
+#include <TH1.h>
+#include <TStyle.h> 
+#include <TMath.h> 
+#include <TCanvas.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 tempTrender::tempTrender(string filePath) {
-	cout << "The user supplied " << filePath << " as the path to the data file." << endl;
-	cout << "You should probably store this information in a member variable of the class. Good luck with the project! :)" << endl;
+	//cout << "The user supplied " << filePath << " as the path to the data file." << endl;
+	//cout << "You should probably store this information in a member variable of the class. Good luck with the project! :)" << endl;	
+	_filePath = filePath;
 }
+
+
+
+
+void tempTrender::tempOnDay(string Day) {
+	
+    ifstream file(_filePath.c_str());
+    
+    //Defining all vaiables we need
+    vector<string> entries;
+    vector<float> temp;
+    
+    string line, t;
+    int i, j=0, day, s;
+    
+    //Check if the file is opened
+    if(file.is_open()) 
+    {
+		//Fill up the vector with the entries
+		while(getline(file, line)) 
+		{
+			entries.push_back(line);
+		}
+	}
+	
+	s = entries.size();
+	//goes through all the entries
+	for(i=0; i<s; i++)
+	{
+		line = entries[i];
+		
+		//get the day from 22th character
+		day = stoi(line.substr(22,3));
+		if (day == Day)
+		{
+			//get the corresponding temperature at the 16th character
+			temp[j]= stof(line.substr(16,3));
+			j=j+1;
+		}
+		
+	}
+	
+	//creating histogram
+	TH1D* Hist = new TH1D("Hist", "Temperature on a given day", 100, -20, 40);
+	
+	
+	//filling up histogram with the values from vector temp
+	for (i=0; i<j; i++)
+	{
+		t=temp[j];
+		Hist->Fill(t);
+	}
+	
+	//drawing histogram
+	Hist->Draw();
+
+	return 0;
+}
+
